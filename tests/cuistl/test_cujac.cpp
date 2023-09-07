b65b7cb030b3a1fd02f9215f0730c86312cba0df
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2023-09-07 09:54:48 +0200

    Add test for inverse matrices

--- tests/cuistl/test_cujac.cpp
+++ tests/cuistl/test_cujac.cpp
@@ -18,7 +18,7 @@
 */
 #include <config.h>
 
-#define BOOST_TEST_MODULE TestCudaSafeCall
+#define BOOST_TEST_MODULE TestCuJac
 #include <boost/mpl/list.hpp>
 #include <boost/test/unit_test.hpp>
 #include <cuda_runtime.h>
@@ -29,16 +29,29 @@
 #include <opm/simulators/linalg/cuistl/CuVector.hpp>
 #include <opm/simulators/linalg/cuistl/detail/cusparse_matrix_operations.hpp>
 #include <opm/simulators/linalg/cuistl/detail/fix_zero_diagonal.hpp>
+#include <string>
+
+template<class T> void CuVecPrinter(Opm::cuistl::CuVector<T> arg, std::string name){
+    std::cout << name << ": ";
+    std::vector<T> v = arg.asStdVector();
+    for (int i = 0; i < v.size(); i++){
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
+}
+template void CuVecPrinter(Opm::cuistl::CuVector<float>, std::string);
+template void CuVecPrinter(Opm::cuistl::CuVector<double>, std::string);
 
 using NumericTypes = boost::mpl::list<double, float>;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(FlattenAndInvertDiagonal, T, NumericTypes)
+BOOST_AUTO_TEST_CASE_TEMPLATE(FlattenAndInvertDiagonalWith3By3Blocks, T, NumericTypes)
 {
-    const int N = 2;
+    const size_t blocksize = 3;
+    const size_t N = 2;
     const int nonZeroes = 3;
-    using M = Dune::FieldMatrix<T, 3, 3>;
+    using M = Dune::FieldMatrix<T, blocksize, blocksize>;
     using SpMatrix = Dune::BCRSMatrix<M>;
-    using Vector = Dune::BlockVector<Dune::FieldVector<T, 3>>;
+    using Vector = Dune::BlockVector<Dune::FieldVector<T, blocksize>>;
     using cujac = Opm::cuistl::CuJac<SpMatrix, Opm::cuistl::CuVector<T>, Opm::cuistl::CuVector<T>>;
     /*
         create a matrix with this shape
@@ -78,15 +91,83 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(FlattenAndInvertDiagonal, T, NumericTypes)
     B[0][1][1][1]=1.0;
     B[0][1][2][2]=1.0;
 
-    B[1][1][0][0]=1.0;
-    B[1][1][1][1]=1.0;
-    B[1][1][2][2]=1.0;
+    B[1][1][0][0]=-1.0;
+    B[1][1][1][1]=-1.0;
+    B[1][1][2][2]=-1.0;
 
     Opm::cuistl::CuSparseMatrix<T> m = Opm::cuistl::CuSparseMatrix<T>::fromMatrix(Opm::cuistl::detail::makeMatrixWithNonzeroDiagonal(B));
-    auto vec = m.getNonZeroValues().data();
-    BOOST_CHECK(true);
-    // TODO call the flattening here and check the results
-    // detail::flatten(nonZeroValues, rowIndices, columnIndices, numberOfRows, detail::to_size_t(blockSize), d_mDiagInv.data());
+    Opm::cuistl::CuVector<T> d_invDiag(blocksize*blocksize*N);
+
+    Opm::cuistl::detail::flatten(m.getNonZeroValues().data(), m.getRowIndices().data(), m.getColumnIndices().data(), N, blocksize, d_invDiag.data());
+
+    std::vector<T> expected_inv_diag{1.0,-2.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,-1.0,0.0,0.0,0.0,-1.0,0.0,0.0,0.0,-1.0};
+    std::vector<T> computed_inv_diag = d_invDiag.asStdVector();
+
+    BOOST_REQUIRE_EQUAL(expected_inv_diag.size(), computed_inv_diag.size());
+    for (size_t i = 0; i < expected_inv_diag.size(); i++){
+        BOOST_CHECK_EQUAL(expected_inv_diag[i], computed_inv_diag[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(FlattenAndInvertDiagonalWith2y2Blocks, T, NumericTypes)
+{
+    const size_t blocksize = 2;
+    const size_t N = 2;
+    const int nonZeroes = 3;
+    using M = Dune::FieldMatrix<T, blocksize, blocksize>;
+    using SpMatrix = Dune::BCRSMatrix<M>;
+    using Vector = Dune::BlockVector<Dune::FieldVector<T, blocksize>>;
+    using cujac = Opm::cuistl::CuJac<SpMatrix, Opm::cuistl::CuVector<T>, Opm::cuistl::CuVector<T>>;
+    /*
+        create a matrix with this shape
+        | |1 2| | 1  0| |
+        | |0 1| | 0  1| |
+        |                | 
+        | |0 0| |-1  0|| |
+        | |0 0| | 0 -1|| |
+
+        we want to end up with this vector
+        | | 1 -2| |
+        | | 0  1| |
+        |         |
+        | |-1  0| |
+        | | 0 -1| |
+        
+    */
+
+    SpMatrix B(N, N, nonZeroes, SpMatrix::row_wise);
+    for (auto row = B.createbegin(); row != B.createend(); ++row) {
+        row.insert(row.index());
+        if (row.index() == 0) {
+            row.insert(row.index() + 1);
+        }
+    }
+
+    B[0][0][0][0]=1.0;
+    B[0][0][0][1]=2.0;
+    B[0][0][1][1]=1.0;
+    B[0][0][2][2]=1.0;
+
+    B[0][1][0][0]=1.0;
+    B[0][1][1][1]=1.0;
+    B[0][1][2][2]=1.0;
+
+    B[1][1][0][0]=-1.0;
+    B[1][1][1][1]=-1.0;
+    B[1][1][2][2]=-1.0;
+
+    Opm::cuistl::CuSparseMatrix<T> m = Opm::cuistl::CuSparseMatrix<T>::fromMatrix(Opm::cuistl::detail::makeMatrixWithNonzeroDiagonal(B));
+    Opm::cuistl::CuVector<T> d_invDiag(blocksize*blocksize*N);
+
+    Opm::cuistl::detail::flatten(m.getNonZeroValues().data(), m.getRowIndices().data(), m.getColumnIndices().data(), N, blocksize, d_invDiag.data());
+
+    std::vector<T> expected_inv_diag{1.0,-2.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0,-1.0,0.0,0.0,0.0,-1.0,0.0,0.0,0.0,-1.0};
+    std::vector<T> computed_inv_diag = d_invDiag.asStdVector();
+
+    BOOST_REQUIRE_EQUAL(expected_inv_diag.size(), computed_inv_diag.size());
+    for (size_t i = 0; i < expected_inv_diag.size(); i++){
+        BOOST_CHECK_EQUAL(expected_inv_diag[i], computed_inv_diag[i]);
+    }
 }
 
 BOOST_AUTO_TEST_CASE(CUJACApplyIsEqualToDuneSeqJacApply)
