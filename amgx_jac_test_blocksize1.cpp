ee124f2f89a42cdf3af0859fcf08e3635e99811b
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2023-09-25 17:39:38 +0200

    Add proof of concept DILU into OPM. Currently DILU is not converging. The code was created by copying the CuJac preconditioner, so alot of artifacts from that remain. Not sure if it is worth cleaning up given how little promise AMGX currently shows.

---
+++ amgx_jac_test_blocksize1.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+#include <cuda_runtime.h>
+#include <amgx_c.h>
+
+int main(){
+    // Initialize AMGX
+    AMGX_SAFE_CALL(AMGX_initialize());
+
+    // Create an AMGX configuration
+    AMGX_config_handle cfg;
+    AMGX_SAFE_CALL(AMGX_config_create(&cfg, "solver=POLYNOMIAL, max_iters=1, relaxation_factor=0.5"));
+
+    // Create AMGX resources
+    AMGX_resources_handle rsrc;
+    AMGX_SAFE_CALL(AMGX_resources_create_simple(&rsrc, cfg));
+
+    // Define the linear system size
+    int num_rows = 4;  
+    int blocksize = 1;
+    int nnz = 8; // number of nonzero elements
+
+    // Create and populate the vector
+    std::vector<double> input_vector_x({1, 2, 1, 1});
+    std::vector<double> input_vector_b({2, 1, 3, 4});
+
+    // Create AMGX vector
+    AMGX_vector_handle x;
+    AMGX_vector_handle b;
+    AMGX_SAFE_CALL(AMGX_vector_create(&x, rsrc, AMGX_mode_dDDI));
+    AMGX_SAFE_CALL(AMGX_vector_create(&b, rsrc, AMGX_mode_dDDI));
+
+    // Upload the vector data to AMGX
+    AMGX_SAFE_CALL(AMGX_vector_upload(x, num_rows, blocksize, input_vector_x.data()));
+    AMGX_SAFE_CALL(AMGX_vector_upload(b, num_rows, blocksize, input_vector_b.data()));
+
+    // Create the solver
+    AMGX_solver_handle solver;
+    AMGX_SAFE_CALL(AMGX_solver_create(&solver, rsrc, AMGX_mode_dDDI, cfg));
+
+    // Setup the linear system (A is not used in this example)
+    AMGX_matrix_handle A;
+    AMGX_SAFE_CALL(AMGX_matrix_create(&A, rsrc, AMGX_mode_dDDI));
+
+    // Set A as an identity matrix
+    std::vector<int> row_ptrs({0,3,6,7,8});
+    std::vector<int> col_indices({0,1,2,0,1,3,2,3});
+    std::vector<double> values({3,1,1,2,1,1,-1,-1});
+
+    // Upload the matrix data to AMGX using AMGX_matrix_upload_all()
+    AMGX_SAFE_CALL(AMGX_matrix_upload_all(A, num_rows, nnz, blocksize, blocksize, row_ptrs.data(), col_indices.data(), values.data(), NULL));
+
+    // Setup the solver
+    AMGX_SAFE_CALL(AMGX_solver_setup(solver, A));
+
+    // Perform Jacobi preconditioning
+    AMGX_SAFE_CALL(AMGX_solver_solve(solver, b, x));
+
+    // Download the result vector from AMGX
+    AMGX_SAFE_CALL(AMGX_vector_download(x, &input_vector_x[0]));
+
+    // Clean up resources
+    AMGX_SAFE_CALL(AMGX_solver_destroy(solver));
+    AMGX_SAFE_CALL(AMGX_matrix_destroy(A));
+    AMGX_SAFE_CALL(AMGX_vector_destroy(x));
+    AMGX_SAFE_CALL(AMGX_vector_destroy(b));
+    AMGX_SAFE_CALL(AMGX_resources_destroy(rsrc));
+    AMGX_SAFE_CALL(AMGX_config_destroy(cfg));
+    AMGX_SAFE_CALL(AMGX_finalize());
+
+    // Print the result
+    for (int i = 0; i < num_rows; ++i) {
+        std::cout << "x[" << i << "] = " << input_vector_x[i] << std::endl;
+    }
+
+    return 0;
+}
---
+++ opm/simulators/linalg/cuistl/CuAmgxDilu.cpp
@@ -0,0 +1,186 @@
+/*
+  Copyright 2022-2023 SINTEF AS
+  This file is part of the Open Porous Media project (OPM).
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <cusparse.h>
+#include <dune/common/fmatrix.hh>
+#include <dune/common/fvector.hh>
+#include <dune/istl/bcrsmatrix.hh>
+#include <dune/istl/bvector.hh>
+#include <fmt/core.h>
+#include <opm/common/ErrorMacros.hpp>
+#include <opm/simulators/linalg/cuistl/CuAmgxDilu.hpp>
+#include <opm/simulators/linalg/cuistl/CuVector.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cusparse_constants.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cusparse_safe_call.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cusparse_wrapper.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cublas_safe_call.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cublas_wrapper.hpp>
+#include <opm/simulators/linalg/cuistl/detail/CuBlasHandle.hpp>
+#include <opm/simulators/linalg/cuistl/detail/fix_zero_diagonal.hpp>
+#include <opm/simulators/linalg/cuistl/detail/safe_conversion.hpp>
+#include <opm/simulators/linalg/cuistl/detail/vector_operations.hpp>
+#include <opm/simulators/linalg/cuistl/PreconditionerAdapter.hpp>
+#include <opm/simulators/linalg/matrixblock.hh>
+
+#include <iostream>
+#include <amgx_c.h>
+
+// This file is based on the guide at https://docs.nvidia.com/cuda/cusparse/index.html#csrilu02_solve ,
+// it highly recommended to read that before proceeding.
+
+namespace Opm::cuistl
+{
+
+template <class M, class X, class Y, int l>
+CuJac<M, X, Y, l>::CuJac(const M& A, field_type w)
+    : m_underlyingMatrix(A)
+    , m_w(w)
+    , m(CuSparseMatrix<field_type>::fromMatrix(detail::makeMatrixWithNonzeroDiagonal(A)))
+    , m_diagInvFlattened(m.N() * m.blockSize()*m.blockSize())
+    , m_description(detail::createMatrixDescription())
+    , m_cuSparseHandle(detail::CuSparseHandle::getInstance())
+    , m_cuBlasHandle(detail::CuBlasHandle::getInstance())
+    , d_resultBuffer(m.N() * m.blockSize())
+{
+    // Some sanity check
+    OPM_ERROR_IF(A.N() != m.N(),
+                 fmt::format("CuSparse matrix not same size as DUNE matrix. {} vs {}.", m.N(), A.N()));
+    OPM_ERROR_IF(
+        A[0][0].N() != m.blockSize(),
+        fmt::format("CuSparse matrix not same blocksize as DUNE matrix. {} vs {}.", m.blockSize(), A[0][0].N()));
+    OPM_ERROR_IF(
+        A.N() * A[0][0].N() != m.dim(),
+        fmt::format("CuSparse matrix not same dimension as DUNE matrix. {} vs {}.", m.dim(), A.N() * A[0][0].N()));
+    OPM_ERROR_IF(A.nonzeroes() != m.nonzeroes(),
+                 fmt::format("CuSparse matrix not same number of non zeroes as DUNE matrix. {} vs {}. ",
+                             m.nonzeroes(),
+                             A.nonzeroes()));
+
+    update();
+    AMGX_SAFE_CALL(AMGX_initialize());
+    std::cout << "AmgxDilu initiated" << std::endl;
+}
+
+template <class M, class X, class Y, int l>
+CuJac<M, X, Y, l>::~CuJac(){
+    
+    AMGX_SAFE_CALL(AMGX_finalize());
+}
+
+template <class M, class X, class Y, int l>
+void
+CuJac<M, X, Y, l>::pre([[maybe_unused]] X& x, [[maybe_unused]] Y& b)
+{
+}
+
+template <class M, class X, class Y, int l>
+void
+CuJac<M, X, Y, l>::apply(X& x, const Y& b)
+{
+    const auto numberOfRows = detail::to_int(m.N());
+    const auto numberOfNonzeroBlocks = detail::to_int(m.nonzeroes());
+    const auto blockSize = detail::to_int(m.blockSize());
+    auto nonZeroValues = m.getNonZeroValues().data();
+    auto rowIndices = m.getRowIndices().data();
+    auto columnIndices = m.getColumnIndices().data();
+
+    
+
+    AMGX_config_handle cfg;
+    AMGX_SAFE_CALL(AMGX_config_create(&cfg, "solver=MULTICOLOR_DILU, max_iters=1, relaxation_factor=0.9")); // TODO insert w
+
+    // Create AMGX resources
+    AMGX_resources_handle rsrc;
+    AMGX_SAFE_CALL(AMGX_resources_create_simple(&rsrc, cfg));
+
+    AMGX_solver_handle amgx_solver;
+    AMGX_SAFE_CALL(AMGX_solver_create(&amgx_solver, rsrc, AMGX_mode_dDDI, cfg));
+
+    // Create vectors usable by amgx
+    // TODO: create allow for using AMGX with floats by havnig AMGX_mode be dynamically chosen
+    AMGX_vector_handle amgx_x; 
+    AMGX_vector_handle amgx_b;
+    AMGX_SAFE_CALL(AMGX_vector_create(&amgx_x, rsrc, AMGX_mode_dDDI));
+    AMGX_SAFE_CALL(AMGX_vector_create(&amgx_b, rsrc, AMGX_mode_dDDI));
+    AMGX_SAFE_CALL(AMGX_vector_upload(amgx_x, numberOfRows, blockSize, x.data()));
+    AMGX_SAFE_CALL(AMGX_vector_upload(amgx_b, numberOfRows, blockSize, b.data()));
+
+    // Create matrix usable by amgx
+    AMGX_matrix_handle amgx_A;
+    AMGX_SAFE_CALL(AMGX_matrix_create(&amgx_A, rsrc, AMGX_mode_dDDI));
+    AMGX_SAFE_CALL(AMGX_matrix_upload_all(amgx_A, numberOfRows, numberOfNonzeroBlocks, blockSize, blockSize, rowIndices, columnIndices, nonZeroValues, NULL));
+
+    // Setup the solver
+    AMGX_SAFE_CALL(AMGX_solver_setup(amgx_solver, amgx_A));
+
+    // Perform Jacobi preconditioning
+    AMGX_SAFE_CALL(AMGX_solver_solve(amgx_solver, amgx_b, amgx_x));
+
+    // Download the result vector from AMGX
+    AMGX_SAFE_CALL(AMGX_vector_download(amgx_x, x.data()));
+
+    AMGX_SAFE_CALL(AMGX_solver_destroy(amgx_solver));
+    AMGX_SAFE_CALL(AMGX_matrix_destroy(amgx_A));
+    AMGX_SAFE_CALL(AMGX_vector_destroy(amgx_x));
+    AMGX_SAFE_CALL(AMGX_vector_destroy(amgx_b));
+    AMGX_SAFE_CALL(AMGX_resources_destroy(rsrc));
+    AMGX_SAFE_CALL(AMGX_config_destroy(cfg));
+}
+
+template <class M, class X, class Y, int l>
+void
+CuJac<M, X, Y, l>::post([[maybe_unused]] X& x)
+{
+}
+
+template <class M, class X, class Y, int l>
+Dune::SolverCategory::Category
+CuJac<M, X, Y, l>::category() const
+{
+    return Dune::SolverCategory::sequential;
+}
+
+template <class M, class X, class Y, int l>
+void
+CuJac<M, X, Y, l>::update()
+{
+    return;
+}
+
+} // namespace Opm::cuistl
+#define INSTANTIATE_CUJAC_DUNE(realtype, blockdim)                                                                 \
+    template class ::Opm::cuistl::CuJac<Dune::BCRSMatrix<Dune::FieldMatrix<realtype, blockdim, blockdim>>,         \
+                                            ::Opm::cuistl::CuVector<realtype>,                                         \
+                                            ::Opm::cuistl::CuVector<realtype>>;                                        \
+    template class ::Opm::cuistl::CuJac<Dune::BCRSMatrix<Opm::MatrixBlock<realtype, blockdim, blockdim>>,          \
+                                            ::Opm::cuistl::CuVector<realtype>,                                         \
+                                            ::Opm::cuistl::CuVector<realtype>>
+
+
+
+INSTANTIATE_CUJAC_DUNE(double, 1);
+INSTANTIATE_CUJAC_DUNE(double, 2);
+INSTANTIATE_CUJAC_DUNE(double, 3);
+INSTANTIATE_CUJAC_DUNE(double, 4);
+INSTANTIATE_CUJAC_DUNE(double, 5);
+INSTANTIATE_CUJAC_DUNE(double, 6);
+
+INSTANTIATE_CUJAC_DUNE(float, 1);
+INSTANTIATE_CUJAC_DUNE(float, 2);
+INSTANTIATE_CUJAC_DUNE(float, 3);
+INSTANTIATE_CUJAC_DUNE(float, 4);
+INSTANTIATE_CUJAC_DUNE(float, 5);
+INSTANTIATE_CUJAC_DUNE(float, 6);
