720ceb7d0281f8bb22226d07e61be436ccd1497c
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2024-05-15 13:43:46 +0200

    add CuBuffer sketch

---
+++ opm/simulators/linalg/cuistl/CuBuffer.cpp
@@ -0,0 +1,270 @@
+/*
+  Copyright 2022-2023 SINTEF AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <cublas_v2.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <algorithm>
+#include <fmt/core.h>
+#include <opm/simulators/linalg/cuistl/CuBuffer.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cublas_safe_call.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cublas_wrapper.hpp>
+#include <opm/simulators/linalg/cuistl/detail/cuda_safe_call.hpp>
+#include <opm/simulators/linalg/cuistl/detail/vector_operations.hpp>
+
+namespace Opm::cuistl
+{
+
+template <class T>
+CuBuffer<T>::CuBuffer(const std::vector<T>& data)
+    : CuBuffer(data.data(), detail::to_int(data.size()))
+{
+}
+
+template <class T>
+CuBuffer<T>::CuBuffer(const size_t numberOfElements)
+    : m_numberOfElements(detail::to_int(numberOfElements))
+    , m_cuBlasHandle(detail::CuBlasHandle::getInstance())
+{
+    OPM_CUDA_SAFE_CALL(cudaMalloc(&m_dataOnDevice, sizeof(T) * detail::to_size_t(m_numberOfElements)));
+}
+
+template <class T>
+CuBuffer<T>::CuBuffer(const T* dataOnHost, const size_t numberOfElements)
+    : CuBuffer(numberOfElements)
+{
+
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(
+        m_dataOnDevice, dataOnHost, detail::to_size_t(m_numberOfElements) * sizeof(T), cudaMemcpyHostToDevice));
+}
+
+template <class T>
+CuBuffer<T>&
+CuBuffer<T>::operator=(T scalar)
+{
+    assertHasElements();
+    detail::setVectorValue(data(), detail::to_size_t(m_numberOfElements), scalar);
+    return *this;
+}
+
+template <class T>
+CuBuffer<T>&
+CuBuffer<T>::operator=(const CuBuffer<T>& other)
+{
+    assertHasElements();
+    assertSameSize(other);
+
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(m_dataOnDevice,
+                                  other.m_dataOnDevice,
+                                  detail::to_size_t(m_numberOfElements) * sizeof(T),
+                                  cudaMemcpyDeviceToDevice));
+    return *this;
+}
+
+template <class T>
+CuBuffer<T>::CuBuffer(const CuBuffer<T>& other)
+    : CuBuffer(other.m_numberOfElements)
+{
+    assertHasElements();
+    assertSameSize(other);
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(m_dataOnDevice,
+                                  other.m_dataOnDevice,
+                                  detail::to_size_t(m_numberOfElements) * sizeof(T),
+                                  cudaMemcpyDeviceToDevice));
+}
+
+template <class T>
+CuBuffer<T>::~CuBuffer()
+{
+    OPM_CUDA_WARN_IF_ERROR(cudaFree(m_dataOnDevice));
+}
+
+template <typename T>
+const T*
+CuBuffer<T>::data() const
+{
+    return m_dataOnDevice;
+}
+
+template <typename T>
+typename CuBuffer<T>::size_type
+CuBuffer<T>::size() const
+{
+    // Note that there is no way for m_numberOfElements to be non-positive,
+    // but for sanity we still use the safe conversion function here.
+    //
+    // We also doubt that this will lead to any performance penalty, but should this prove
+    // to be false, this can be replaced by a simple cast to size_t
+    return detail::to_size_t(m_numberOfElements);
+}
+
+template <typename T>
+void
+CuBuffer<T>::resize(int newSize)
+{
+    if (newSize < 0) {
+        OPM_THROW(std::invalid_argument, "Setting a CuBuffer size to a negative number is not allowed");
+    }
+    // Allocate memory for the new buffer
+    T* tmpBuffer = nullptr;
+    OPM_CUDA_SAFE_CALL(cudaMalloc(&tmpBuffer, sizeof(T) * detail::to_size_t(newSize)));
+
+    // Move the data from the old to the new buffer with truncation
+    int sizeOfMove = std::min({m_numberOfElements, newSize});
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(tmpBuffer,
+                                  m_dataOnDevice,
+                                  detail::to_size_t(sizeOfMove) * sizeof(T),
+                                  cudaMemcpyDeviceToDevice));
+
+    // free the old buffer
+    OPM_CUDA_SAFE_CALL(cudaFree(m_dataOnDevice));
+
+    // swap the buffers
+    m_dataOnDevice = tmpBuffer;
+
+    // update size
+    m_numberOfElements = newSize;
+}
+
+template <typename T>
+std::vector<T>
+CuBuffer<T>::asStdVector() const
+{
+    std::vector<T> temporary(detail::to_size_t(m_numberOfElements));
+    copyToHost(temporary);
+    return temporary;
+}
+
+template <typename T>
+void
+CuBuffer<T>::setZeroAtIndexSet(const CuBuffer<int>& indexSet)
+{
+    detail::setZeroAtIndexSet(m_dataOnDevice, indexSet.size(), indexSet.data());
+}
+
+template <typename T>
+void
+CuBuffer<T>::assertSameSize(const CuBuffer<T>& x) const
+{
+    assertSameSize(x.m_numberOfElements);
+}
+
+template <typename T>
+void
+CuBuffer<T>::assertSameSize(int size) const
+{
+    if (size != m_numberOfElements) {
+        OPM_THROW(std::invalid_argument,
+                  fmt::format("Given vector has {}, while we have {}.", size, m_numberOfElements));
+    }
+}
+
+template <typename T>
+void
+CuBuffer<T>::assertHasElements() const
+{
+    if (m_numberOfElements <= 0) {
+        OPM_THROW(std::invalid_argument, "We have 0 elements");
+    }
+}
+
+template <typename T>
+T*
+CuBuffer<T>::data()
+{
+    return m_dataOnDevice;
+}
+
+template <typename T>
+T
+CuBuffer<T>::front()
+{
+#ifndef NDEBUG
+    if (m_numberOfElements < 1) {
+        OPM_THROW(std::invalid_argument,
+                  fmt::format("Can not fetch the front item of a CuBuffer with no elements"));
+    }
+#endif
+    return m_dataOnDevice[0];
+}
+
+template <typename T>
+T
+CuBuffer<T>::back()
+{
+#ifndef NDEBUG
+    if (m_numberOfElements < 1) {
+        OPM_THROW(std::invalid_argument,
+                  fmt::format("Can not fetch the back item of a CuBuffer with no elements"));
+    }
+#endif
+    return m_dataOnDevice[m_numberOfElements-1];
+}
+
+template <class T>
+void
+CuBuffer<T>::copyFromHost(const T* dataPointer, size_t numberOfElements)
+{
+    if (numberOfElements > size()) {
+        OPM_THROW(std::runtime_error,
+                  fmt::format("Requesting to copy too many elements. Vector has {} elements, while {} was requested.",
+                              size(),
+                              numberOfElements));
+    }
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(data(), dataPointer, numberOfElements * sizeof(T), cudaMemcpyHostToDevice));
+}
+
+template <class T>
+void
+CuBuffer<T>::copyToHost(T* dataPointer, size_t numberOfElements) const
+{
+    assertSameSize(detail::to_int(numberOfElements));
+    OPM_CUDA_SAFE_CALL(cudaMemcpy(dataPointer, data(), numberOfElements * sizeof(T), cudaMemcpyDeviceToHost));
+}
+
+template <class T>
+void
+CuBuffer<T>::copyFromHost(const std::vector<T>& data)
+{
+    copyFromHost(data.data(), data.size());
+}
+template <class T>
+void
+CuBuffer<T>::copyToHost(std::vector<T>& data) const
+{
+    copyToHost(data.data(), data.size());
+}
+
+template <typename T>
+void
+CuBuffer<T>::prepareSendBuf(CuBuffer<T>& buffer, const CuBuffer<int>& indexSet) const
+{
+    return detail::prepareSendBuf(m_dataOnDevice, buffer.data(), indexSet.size(), indexSet.data());
+}
+template <typename T>
+void
+CuBuffer<T>::syncFromRecvBuf(CuBuffer<T>& buffer, const CuBuffer<int>& indexSet) const
+{
+    return detail::syncFromRecvBuf(m_dataOnDevice, buffer.data(), indexSet.size(), indexSet.data());
+}
+
+template class CuBuffer<double>;
+template class CuBuffer<float>;
+template class CuBuffer<int>;
+
+} // namespace Opm::cuistl
---
+++ opm/simulators/linalg/cuistl/CuBuffer.hpp
@@ -0,0 +1,326 @@
+/*
+  Copyright 2022-2023 SINTEF AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef OPM_CUBUFFER_HEADER_HPP
+#define OPM_CUBUFFER_HEADER_HPP
+#include <dune/common/fvector.hh>
+#include <dune/istl/bvector.hh>
+#include <exception>
+#include <fmt/core.h>
+#include <opm/common/ErrorMacros.hpp>
+#include <opm/simulators/linalg/cuistl/detail/CuBlasHandle.hpp>
+#include <opm/simulators/linalg/cuistl/detail/safe_conversion.hpp>
+#include <vector>
+#include <string>
+
+
+namespace Opm::cuistl
+{
+
+/**
+ * @brief The CuBuffer class is a simple container class for the GPU.
+ *
+ *
+ * Example usage:
+ *
+ * @code{.cpp}
+ * #include <opm/simulators/linalg/cuistl/CuBuffer.hpp>
+ *
+ * void someFunction() {
+ *     auto someDataOnCPU = std::vector<double>({1.0, 2.0, 42.0, 59.9451743, 10.7132692});
+ *
+ *     auto dataOnGPU = CuBuffer<double>(someDataOnCPU);
+ *
+ *     // Get data back on CPU in another vector:
+ *     auto stdVectorOnCPU = dataOnGPU.asStdVector();
+ * }
+ *
+ * @tparam T the type to store. Can be either float, double or int.
+ */
+template <typename T>
+class CuBuffer
+{
+public:
+    using field_type = T;
+    using size_type = size_t;
+
+
+    /**
+     * @brief CuBuffer allocates new GPU memory of the same size as other and copies the content of the other vector to
+     * this newly allocated memory.
+     *
+     * @note This does synchronous transfer.
+     *
+     * @param other the vector to copy from
+     */
+    CuBuffer(const CuBuffer<T>& other);
+
+    /**
+     * @brief CuBuffer allocates new GPU memory of the same size as data and copies the content of the data vector to
+     * this newly allocated memory.
+     *
+     * @note This does CPU to GPU transfer.
+     * @note This does synchronous transfer.
+     *
+     * @note For now data.size() needs to be within the limits of int due to restrctions of CuBlas.
+     *
+     * @param data the vector to copy from
+     */
+    explicit CuBuffer(const std::vector<T>& data);
+
+    /**
+     * @brief operator= copies the content of the data vector to the memory of this vector.
+     *
+     * @note This requires the two vectors to be of the same size.
+     * @note This does synchronous transfer.
+     *
+     * @param other the vector to copy from
+     */
+    CuBuffer& operator=(const CuBuffer<T>& other);
+
+    /**
+     * @brief operator= sets the whole vector equal to the scalar value.
+     *
+     * @note This does asynchronous operations
+     *
+     * @param scalar the value all elements will be set to.
+     */
+    CuBuffer& operator=(T scalar);
+
+    /**
+     * @brief CuBuffer allocates new GPU memory of size numberOfElements * sizeof(T)
+     *
+     * @note For now numberOfElements needs to be within the limits of int due to restrictions in cublas
+     *
+     * @param numberOfElements number of T elements to allocate
+     */
+    explicit CuBuffer(const size_t numberOfElements);
+
+
+    /**
+     * @brief CuBuffer allocates new GPU memory of size numberOfElements * sizeof(T) and copies numberOfElements from
+     * data
+     *
+     * @note This assumes the data is on the CPU.
+     *
+     * @param numberOfElements number of T elements to allocate
+     * @param dataOnHost data on host/CPU
+     *
+     * @note For now numberOfElements needs to be within the limits of int due to restrictions in cublas
+     */
+    CuBuffer(const T* dataOnHost, const size_t numberOfElements);
+
+    /**
+     * @brief ~CuBuffer calls cudaFree
+     */
+    virtual ~CuBuffer();
+
+    /**
+     * @return the raw pointer to the GPU data
+     */
+    T* data();
+
+    /**
+     * @return fetch the first element in a CuBuffer
+     */
+    T front();
+
+    /**
+     * @return fetch the last element in a CuBuffer
+     */
+    T back();
+
+    /**
+     * @return the raw pointer to the GPU data
+     */
+    const T* data() const;
+
+    /**
+     * @return fetch the first element in a CuBuffer
+     */
+    const T front() const;
+
+    /**
+     * @return fetch the last element in a CuBuffer
+     */
+    const T back() const;
+
+    /**
+     * @brief copyFromHost copies data from a Dune::BlockVector
+     * @param bvector the vector to copy from
+     *
+     * @note This does synchronous transfer.
+     * @note This assumes that the size of this vector is equal to the size of the input vector.
+     */
+    template <int BlockDimension>
+    void copyFromHost(const Dune::BlockVector<Dune::FieldVector<T, BlockDimension>>& bvector)
+    {
+        // TODO: [perf] vector.size() can be replaced by bvector.N() * BlockDimension
+        if (detail::to_size_t(m_numberOfElements) != bvector.size()) {
+            OPM_THROW(std::runtime_error,
+                      fmt::format("Given incompatible vector size. CuBuffer has size {}, \n"
+                                  "however, BlockVector has N() = {}, and size = {}.",
+                                  m_numberOfElements,
+                                  bvector.N(),
+                                  bvector.size()));
+        }
+        const auto dataPointer = static_cast<const T*>(&(bvector[0][0]));
+        copyFromHost(dataPointer, m_numberOfElements);
+    }
+
+    /**
+     * @brief copyToHost copies data to a Dune::BlockVector
+     * @param bvector the vector to copy to
+     *
+     * @note This does synchronous transfer.
+     * @note This assumes that the size of this vector is equal to the size of the input vector.
+     */
+    template <int BlockDimension>
+    void copyToHost(Dune::BlockVector<Dune::FieldVector<T, BlockDimension>>& bvector) const
+    {
+        // TODO: [perf] vector.size() can be replaced by bvector.N() * BlockDimension
+        if (detail::to_size_t(m_numberOfElements) != bvector.size()) {
+            OPM_THROW(std::runtime_error,
+                      fmt::format("Given incompatible vector size. CuBuffer has size {},\n however, the BlockVector "
+                                  "has has N() = {}, and size() = {}.",
+                                  m_numberOfElements,
+                                  bvector.N(),
+                                  bvector.size()));
+        }
+        const auto dataPointer = static_cast<T*>(&(bvector[0][0]));
+        copyToHost(dataPointer, m_numberOfElements);
+    }
+
+    /**
+     * @brief copyFromHost copies numberOfElements from the CPU memory dataPointer
+     * @param dataPointer raw pointer to CPU memory
+     * @param numberOfElements number of elements to copy
+     * @note This does synchronous transfer.
+     * @note assumes that this vector has numberOfElements elements
+     */
+    void copyFromHost(const T* dataPointer, size_t numberOfElements);
+
+    /**
+     * @brief copyFromHost copies numberOfElements to the CPU memory dataPointer
+     * @param dataPointer raw pointer to CPU memory
+     * @param numberOfElements number of elements to copy
+     * @note This does synchronous transfer.
+     * @note assumes that this vector has numberOfElements elements
+     */
+    void copyToHost(T* dataPointer, size_t numberOfElements) const;
+
+    /**
+     * @brief copyToHost copies data from an std::vector
+     * @param data the vector to copy from
+     *
+     * @note This does synchronous transfer.
+     * @note This assumes that the size of this vector is equal to the size of the input vector.
+     */
+    void copyFromHost(const std::vector<T>& data);
+
+    /**
+     * @brief copyToHost copies data to an std::vector
+     * @param data the vector to copy to
+     *
+     * @note This does synchronous transfer.
+     * @note This assumes that the size of this vector is equal to the size of the input vector.
+     */
+    void copyToHost(std::vector<T>& data) const;
+
+    void prepareSendBuf(CuBuffer<T>& buffer, const CuBuffer<int>& indexSet) const;
+    void syncFromRecvBuf(CuBuffer<T>& buffer, const CuBuffer<int>& indexSet) const;
+
+    /**
+     * @brief size returns the size (number of T elements) in the vector
+     * @return number of elements
+     */
+    size_type size() const;
+
+    /**
+     * @brief resize the number of elements that fit in the vector, shrinking it causes truncation
+     * @param number of elements in the new buffer
+     */
+    void resize(int);
+
+    /**
+     * @brief creates an std::vector of the same size and copies the GPU data to this std::vector
+     * @return an std::vector containing the elements copied from the GPU.
+     */
+    std::vector<T> asStdVector() const;
+
+    /**
+     * @brief creates an std::vector of the same size and copies the GPU data to this std::vector
+     * @return an std::vector containing the elements copied from the GPU.
+     */
+    template <int blockSize>
+    Dune::BlockVector<Dune::FieldVector<T, blockSize>> asDuneBlockVector() const
+    {
+        OPM_ERROR_IF(size() % blockSize != 0,
+                     fmt::format("blockSize is not a multiple of size(). Given blockSize = {}, and size() = {}",
+                                 blockSize,
+                                 size()));
+
+        Dune::BlockVector<Dune::FieldVector<T, blockSize>> returnValue(size() / blockSize);
+        copyToHost(returnValue);
+        return returnValue;
+    }
+
+
+    /**
+     * @brief setZeroAtIndexSet for each element in indexSet, sets the index of this vector to be zero
+     * @param indexSet the set of indices to set to zero
+     *
+     * @note Assumes all indices are within range
+     *
+     * This is supposed to do the same as the following code on the CPU:
+     * @code{.cpp}
+     * for (int index : indexSet) {
+     *     this->data[index] = T(0.0);
+     * }
+     * @endcode
+     */
+    void setZeroAtIndexSet(const CuBuffer<int>& indexSet);
+
+    // Slow method that creates a string representation of a CuBuffer for debug purposes
+    std::string toDebugString()
+    {
+        std::vector<T> v = asStdVector();
+        std::string res = "";
+        for (T element : v){
+            res += std::to_string(element) + " ";
+        }
+        res += std::to_string(v[v.size()-1]);
+        return res;
+    }
+
+private:
+    T* m_dataOnDevice = nullptr;
+
+    // Note that we store this as int to make sure we are always cublas compatible.
+    // This gives the added benefit that a size_t to int conversion error occurs during construction.
+    int m_numberOfElements;
+    detail::CuBlasHandle& m_cuBlasHandle;
+
+    void assertSameSize(const CuBuffer<T>& other) const;
+    void assertSameSize(int size) const;
+
+    void assertHasElements() const;
+};
+
+} // namespace Opm::cuistl
+#endif
