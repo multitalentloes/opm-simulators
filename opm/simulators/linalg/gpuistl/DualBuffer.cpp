ca55e55e150526d22a16b03f92958948a5a8dbe7
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2025-04-30 09:31:29 +0200

    WIP

---
+++ opm/simulators/linalg/gpuistl/DualBuffer.cpp
@@ -0,0 +1,33 @@
+/*
+  Copyright 2024 SINTEF AS
+
+  This file is part of the Open Porous Media project (OPM).
+
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <algorithm>
+#include <fmt/core.h>
+#include <opm/simulators/linalg/gpuistl/DualBuffer.hpp>
+#include <opm/simulators/linalg/gpuistl/detail/gpu_safe_call.hpp>
+
+namespace Opm::gpuistl
+{
+
+template class DualBuffer<double>;
+template class DualBuffer<float>;
+template class DualBuffer<int>;
+
+} // namespace Opm::gpuistl
