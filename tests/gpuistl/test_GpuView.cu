a51aeb80e885bb463a85ceae9335215cf1ed45c1
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2025-05-13 14:48:08 +0200

    minor fixes to compile full flowproblem test

--- tests/gpuistl/test_GpuView.cu
+++ tests/gpuistl/test_GpuView.cu
@@ -1,36 +1,41 @@
-// /*
-//   Copyright 2024 SINTEF AS
+/*
+  Copyright 2024 SINTEF AS
 
-//   This file is part of the Open Porous Media project (OPM).
+  This file is part of the Open Porous Media project (OPM).
 
-//   OPM is free software: you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation, either version 3 of the License, or
-//   (at your option) any later version.
+  OPM is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-//   OPM is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
+  OPM is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-//   You should have received a copy of the GNU General Public License
-//   along with OPM.  If not, see <http://www.gnu.org/licenses/>.
-// */
-// #include <config.h>
+  You should have received a copy of the GNU General Public License
+  along with OPM.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <config.h>
 
-// #define BOOST_TEST_MODULE TestGpuView
+#define BOOST_TEST_MODULE TestGpuView
 
-// #include <boost/test/unit_test.hpp>
-// #include <cuda_runtime.h>
-// #include <dune/common/fvector.hh>
-// #include <dune/istl/bvector.hh>
-// #include <opm/simulators/linalg/gpuistl/GpuView.hpp>
-// #include <opm/simulators/linalg/gpuistl/GpuBuffer.hpp>
-// #include <opm/simulators/linalg/gpuistl/detail/gpu_safe_call.hpp>
-// #include <random>
-// #include <array>
-// #include <algorithm>
-// #include <type_traits>
+#include <boost/test/unit_test.hpp>
+#include <cuda_runtime.h>
+#include <dune/common/fvector.hh>
+#include <dune/istl/bvector.hh>
+#include <opm/simulators/linalg/gpuistl/GpuView.hpp>
+#include <opm/simulators/linalg/gpuistl/GpuBuffer.hpp>
+#include <opm/simulators/linalg/gpuistl/detail/gpu_safe_call.hpp>
+#include <random>
+#include <array>
+#include <algorithm>
+#include <type_traits>
+
+BOOST_AUTO_TEST_CASE(Tautology)
+{
+    BOOST_CHECK(true);
+}
 
 // using GpuViewDouble = ::Opm::gpuistl::GpuView<double>;
 // using GpuBufferDouble = ::Opm::gpuistl::GpuBuffer<double>;
