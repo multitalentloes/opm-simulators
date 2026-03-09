70d4dd0247c08fe883e7d66f10009612af04d0c2
Tobias Meyer Andersen <tobiasmeyer.andersen@sintef.no>
2026-03-09 15:00:57 +0100

    make copy to gpu more efficient again

--- opm/models/blackoil/blackoildiffusionmodule.hh
+++ opm/models/blackoil/blackoildiffusionmodule.hh
@@ -448,7 +448,7 @@ public:
             return *this;
         }
 
-        if (FluidSystem::enableDiffusion()) {
+        if constexpr (getPropValue<TypeTag, Properties::EnableDiffusion>()) {
             tortuosity_ = rhs.tortuosity_;
             diffusionCoefficient_ = rhs.diffusionCoefficient_;
         }
